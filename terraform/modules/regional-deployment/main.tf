02b19691b3a615ec2ed1f271b8ea568e56f6e139
Matthias Wright <matthias.s.wright@gmail.com>
2025-08-01 02:20:25 +0800

    chore: open 9092 ports on ec2 instances

--- terraform/modules/regional-deployment/main.tf
+++ terraform/modules/regional-deployment/main.tf
@@ -125,6 +125,13 @@ resource "aws_security_group" "docker" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 9092
+    to_port     = 9092
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
